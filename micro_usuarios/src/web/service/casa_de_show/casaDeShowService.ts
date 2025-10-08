15ef64358adb4cf81ea983af8ebbf96d6e44feaf
Carlos Eduardo <eduardorodrigues@gmail.com>
2025-10-07 21:45:00 -0300

    servicos implemnetados

--- micro_usuarios/src/web/service/casa_de_show/casaDeShowService.ts
+++ micro_usuarios/src/web/service/casa_de_show/casaDeShowService.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../../libs/prismaClient";
+import { CasaDeShowDTO } from "../../types/casa_de_show_dtos/casaDeShowDTO";
 
 export async function cadastro(dados: {
   nome: string;
@@ -84,4 +85,98 @@ export async function login(email: string, senha: string) {
       nome_fantasia: casa.nome_fantasia,
     },
   };
+}
+
+export async function listarCasasDeShow() {
+  const lista = await prisma.casaShow.findMany({
+    include: {
+      usuario: true,
+    },
+  });
+
+  return lista.map((casa) => ({
+    id: casa.id_usuario,
+    nome: casa.usuario.nome,
+  }));
+}
+
+export async function buscarCasaDeShowPorId(id: string) {
+  const casa = await prisma.casaShow.findUnique({
+    where: { id_usuario: id },
+    include: {
+      usuario: true,
+    },
+  });
+
+  if (!casa) {
+    return null;
+  }
+
+  return casa;
+}
+
+export async function cadastrarCasa(casaDTO: CasaDeShowDTO) {
+  const novaCasa = await prisma.casaShow.create({
+    data: {
+      nome_fantasia: casaDTO.nome_fantasia,
+      cnpj: casaDTO.cnpj,
+      capacidade: casaDTO.capacidade,
+      endereco: casaDTO.endereco,
+      bairro: casaDTO.bairro,
+      estado: casaDTO.estado,
+      cep: casaDTO.cep,
+      geo_lat: casaDTO.geo_lat,
+      geo_lng: casaDTO.geo_lng,
+      usuario: {
+        create: {
+          nome: casaDTO.usuario[0].nome,
+          email: casaDTO.usuario[0].email,
+          senha_hash: casaDTO.usuario[0].senha_hash,
+          telefone: casaDTO.usuario[0].telefone,
+          tipo: "CASASHOW",
+        },
+      },
+    },
+    include: { usuario: true },
+  });
+
+  return novaCasa;
+}
+
+export async function editarCasa(id_casa: string, casaDTO: CasaDeShowDTO) {
+  const casaAtualizada = await prisma.casaShow.update({
+    where: { id_usuario: id_casa },
+    data: {
+      nome_fantasia: casaDTO.nome_fantasia,
+      cnpj: casaDTO.cnpj,
+      capacidade: casaDTO.capacidade,
+      endereco: casaDTO.endereco,
+      bairro: casaDTO.bairro,
+      estado: casaDTO.estado,
+      cep: casaDTO.cep,
+      geo_lat: casaDTO.geo_lat,
+      geo_lng: casaDTO.geo_lng,
+      usuario: {
+        update: {
+          nome: casaDTO.usuario[0].nome,
+          email: casaDTO.usuario[0].email,
+          senha_hash: casaDTO.usuario[0].senha_hash,
+          telefone: casaDTO.usuario[0].telefone,
+        },
+      },
+    },
+    include: { usuario: true },
+  });
+
+  return casaAtualizada;
+}
+
+export async function deletarCasa(id_casa: string) {
+  if (!id_casa) {
+    throw new Error("ID da casa de show é obrigatório para deletar.");
+  }
+
+  return await prisma.casaShow.delete({
+    where: { id_usuario: id_casa },
+  });
 }
