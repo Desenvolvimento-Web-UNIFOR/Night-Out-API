359ac415f9c9024c5cadf481f2a82916039bed54
Carlos Eduardo <eduardorodrigues@gmail.com>
2025-11-12 17:53:22 -0300

    implementação de correção

--- micro_eventos/src/web/controller/proposta_casa/propostaCasaLCController.ts
+++ micro_eventos/src/web/controller/proposta_casa/propostaCasaLCController.ts
@@ -1,59 +1,66 @@
-import type { RequestHandler } from 'express';
+import type { RequestHandler } from "express";
 import * as propostaCasaService from "../../service/proposta_casa/propostaCasaService";
 import type { PropostaCasaDTO } from "../../types/proposta_casa_dtos/propostaCasaDTO";
 
 export const criarPropostaCasa: RequestHandler = async (req, res) => {
-    try {
-        const dados: PropostaCasaDTO = req.body;
+  try {
+    const dados: PropostaCasaDTO = req.body;
 
-        const novaPropostaCasa = await propostaCasaService.criarPropostaCasa(dados);
+    const novaPropostaCasa = await propostaCasaService.criarPropostaCasa(dados);
 
-        res.status(201).json({
-            message: "Proposta casa criada com sucesso",
-            propostaCasa: novaPropostaCasa,
-        });
-    } catch (e) {
-        res.status(500).json({message: `Erro ao criar proposta casa: ${e}`});
-    }
-}
+    res.status(201).json({
+      message: "Proposta casa criada com sucesso",
+      propostaCasa: novaPropostaCasa,
+    });
+  } catch (e) {
+    res.status(500).json({ message: `Erro ao criar proposta casa: ${e}` });
+  }
+};
 
 export const listarPropostaCasa: RequestHandler = async (req, res) => {
-    try {
-        const lista = await propostaCasaService.listarPropostaCasa();
-        res.status(200).json(lista);
-    } catch (e) {
-        res.status(500).json({message: `Erro ao listar proposta casa: ${e}`});
-    }
-}
+  const { page = 1, pageSize = 10 } = req.query;
+  const offset = (Number(page) - 1) * Number(pageSize);
+
+  try {
+    const lista = await propostaCasaService.listarPropostaCasa({
+      offset,
+      limit: Number(pageSize),
+    });
+    res.status(200).json(lista);
+  } catch (e) {
+    res.status(500).json({ message: `Erro ao listar proposta casa: ${e}` });
+  }
+};
 
 export const buscarPropostaCasaPorId: RequestHandler = async (req, res) => {
-    try {
-        const {id} = req.body;
-        const propostaCasa = await propostaCasaService.buscarPropostaCasa(id);
+  try {
+    const { id } = req.params;
+    const propostaCasa = await propostaCasaService.buscarPropostaCasa(id);
 
-        if(!propostaCasa) {
-            res.status(404).json({message: "Proposta casa não encontrado"});
-            return;
-        }
-
-        res.status(200).json(propostaCasa);
-    } catch (e) {
-        res.status(500).json({message: `Erro ao buscar proposta casa: ${e}`});
+    if (!propostaCasa) {
+      res.status(404).json({ message: "Proposta casa não encontrado" });
+      return;
     }
-}
+
+    res.status(200).json(propostaCasa);
+  } catch (e) {
+    res.status(500).json({ message: `Erro ao buscar proposta casa: ${e}` });
+  }
+};
 
 export const editarPropostaCasa: RequestHandler = async (req, res) => {
-    try {
-        const {id} = req.body;
-        const dados: Partial<PropostaCasaDTO> = req.body;
+  try {
+    const { id } = req.params;
+    const dados: Partial<PropostaCasaDTO> = req.body;
 
-        const propostaCasaAtualizado = await propostaCasaService.atualizarPropostaCasa(id, dados);
+    const propostaCasaAtualizado =
+      await propostaCasaService.atualizarPropostaCasa(id, dados);
 
-        res.status(200).json({
-            message: "Proposta casa atualizado",
-            propostaCasa: propostaCasaAtualizado,
-        });
-    } catch (e) {
-        res.status(500).json({message: `Erro ao editar proposta casa: ${e}`});
-    }
-}
+    res.status(200).json({
+      message: "Proposta casa atualizado",
+      propostaCasa: propostaCasaAtualizado,
+    });
+  } catch (e) {
+    res.status(500).json({ message: `Erro ao editar proposta casa: ${e}` });
+  }
+};
