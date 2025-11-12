62a1d396ead5c226ee71e57f87194cb5d70fcbee
Carlos Eduardo <eduardorodrigues@gmail.com>
2025-11-12 20:04:09 -0300

    implementação de correção

--- micro_eventos/src/core/generated/prisma/index.js
+++ micro_eventos/src/core/generated/prisma/index.js
@@ -194,7 +194,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../prisma",
   "clientVersion": "6.16.2",
@@ -222,8 +223,8 @@ const fs = require('fs')
 config.dirname = __dirname
 if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
   const alternativePaths = [
-    "../generated/prisma",
-    "generated/prisma",
+    "src/core/generated/prisma",
+    "core/generated/prisma",
   ]
   
   const alternativePath = alternativePaths.find((altPath) => {
@@ -253,7 +254,7 @@ Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
 path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
-path.join(process.cwd(), "../generated/prisma/libquery_engine-darwin-arm64.dylib.node")
+path.join(process.cwd(), "src/core/generated/prisma/libquery_engine-darwin-arm64.dylib.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
-path.join(process.cwd(), "../generated/prisma/schema.prisma")
+path.join(process.cwd(), "src/core/generated/prisma/schema.prisma")
