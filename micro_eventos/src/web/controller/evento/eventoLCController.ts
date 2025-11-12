da50b7039a717d037d5d9728c447753f95ff5653
Pedro Ivo <pedroivoivo@hotmail.com>
2025-11-12 17:35:08 -0300

    Ajusta controller eventos

--- micro_eventos/src/web/controller/evento/eventoLCController.ts
+++ micro_eventos/src/web/controller/evento/eventoLCController.ts
@@ -10,16 +10,22 @@ export const criarEvento: RequestHandler = async (req, res) => {
 
         res.status(201).json(novoEvento);
     } catch (e) {
-        res.status(500).json({message: `Erro ao criar evento: ${e}`});
+        res.status(500).json({message: Erro ao criar evento: ${e}});
     }
 };
 
 export const listarEvento: RequestHandler = async (req, res) => {
+    const { page = 1, pageSize = 10 } = req.query;
+    const offset = (Number(page) - 1) * Number(pageSize);
+
     try {
-        const lista = await eventoService.listarEvento();
+        const lista = await eventoService.listarEvento({
+            offset,
+            limit: Number(pageSize),
+        });
         res.status(200).json(lista);
     } catch (e) {
-        res.status(500).json({message: `Erro ao listar ingressos: ${e}`})
+        res.status(500).json({message: Erro ao listar eventos: ${e}})
     }
 };
 
@@ -35,7 +41,7 @@ export const buscarEventoPorId: RequestHandler = async (req, res) => {
 
         res.status(200).json(evento);
     } catch (e) {
-        res.status(500).json({message: `Erro ao buscar evento: ${e}`});
+        res.status(500).json({message: Erro ao buscar evento: ${e}});
     }
 };
 
@@ -48,6 +54,18 @@ export const editarEvento: RequestHandler = async (req, res) => {
 
         res.status(200).json(eventoAtualizado);
     } catch (e) {
-        res.status(500).json({message: `Erro ao editar evento: ${e}`});
+        res.status(500).json({message: Erro ao editar evento: ${e}});
+    }
+};
+
+export const deletarEvento: RequestHandler = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const resultado = await eventoService.deletarEvento(id);
+
+        res.status(200).json(resultado);
+    } catch (e) {
+        res.status(500).json({message: Erro ao deletar evento: ${e}});
     }
 };
