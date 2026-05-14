f96917b7cafd29b44f202fa08c5dd67bef36e1ec
Carlos Eduardo <eduardorodrigues@gmail.com>
2026-05-14 15:51:16 -0300

    fixed

--- micro_eventos/src/web/types/proposta_casa_dtos/propostaCasaDTO.ts
+++ micro_eventos/src/web/types/proposta_casa_dtos/propostaCasaDTO.ts
@@ -3,7 +3,7 @@ import type { EventoDTO } from "../evento_dtos/eventoDTO";
 export interface PropostaCasaDTO {
   id_proposta_casa: string;
   id_artista: string;
-  id_casa_show?: string;
+  id_casa_show: string;
   id_evento: string;
   data_proposta: Date;
   data_evento: Date;
