4341d705c5fd5eee0170aec9ebbe412fc2be601e
Carlos Eduardo <eduardorodrigues@gmail.com>
2026-05-14 15:32:31 -0300

    fixed

--- micro_eventos/src/core/generated/prisma/index-browser.js
+++ micro_eventos/src/core/generated/prisma/index-browser.js
@@ -20,12 +20,12 @@ exports.Prisma = Prisma
 exports.$Enums = {}
 
 /**
- * Prisma Client JS version: 6.17.0
- * Query Engine version: c0aafc03b8ef6cdced8654b9a817999e02457d6a
+ * Prisma Client JS version: 6.16.2
+ * Query Engine version: 1c57fdcd7e44b29b9313256c76699e91c3ac3c43
  */
 Prisma.prismaVersion = {
-  client: "6.17.0",
-  engine: "c0aafc03b8ef6cdced8654b9a817999e02457d6a"
+  client: "6.16.2",
+  engine: "1c57fdcd7e44b29b9313256c76699e91c3ac3c43"
 }
 
 Prisma.PrismaClientKnownRequestError = () => {
@@ -136,6 +136,7 @@ exports.Prisma.EventoScalarFieldEnum = {
 exports.Prisma.PropostaCasaScalarFieldEnum = {
   id_proposta_casa: 'id_proposta_casa',
   id_artista: 'id_artista',
+  id_casa_show: 'id_casa_show',
   id_evento: 'id_evento',
   data_proposta: 'data_proposta',
   data_evento: 'data_evento',
---
+++ micro_eventos/src/core/prisma/migrations/20260514181146_npx_prisma_generate_schema_src_core_prisma/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."PropostaArtista" ADD COLUMN     "id_artista" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."PropostaCasa" ADD COLUMN     "id_casa_show" TEXT;
